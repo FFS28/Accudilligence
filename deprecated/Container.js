1cff9b56d3438227581a919e055c071d1804dd77
FFS28 <roylevisay28@gmail.com>
2022-11-05 00:46:10 -0700

    set deprecated

---
+++ deprecated/Container.js
@@ -0,0 +1,17 @@
+/* src/Container.js */
+import React from 'react'
+
+const Container = ({ children }) => (
+  <div style={styles.container}>
+    { children }
+  </div>
+)
+
+const styles = {
+  container: {
+    margin: '0 auto',
+    padding: '50px 100px'
+  }
+}
+
+export default Container
